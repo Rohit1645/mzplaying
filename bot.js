e730da4fcddca7da26bfb4fdbb3eea9d2f401421
GMZN-Host <43391576+GMZN-Hostt@users.noreply.github.com>
2019-04-13 12:39:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,51 +9,36 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const UserBlocked = new Set();
-const moment = require('moment');
-const jimp = require('jimp');
-const Canvas = require('canvas');
-const prefix = '*'
+const prefix = '!'
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot with GMZN Host')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+});
+ 
+ 
+ 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : GMZN Host`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : @ GMZN Host ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setStatus("online")
+ 
 });
 
-//--TEST--
 
-
-const adminprefix = "!";
-const devs = ['543119759495921664', 'none'];
- 
-
+const adminprefix = "!";//تذكير نغير البرفكس
+const devs = ['543119759495921664','543119759495921664'];//zمهم نحط الايدي
 client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
+  var argresult = message.content.split(` `).slice(1).join(' '); //حقوق GMZN Host
     if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'ply')) {
+   
+if (message.content.startsWith(adminprefix + 'ply')) { //حقوق GMZN Host
   client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else 
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`) //حقوق GMZN Host
+} else
 if (message.content.startsWith(adminprefix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
@@ -62,21 +47,11 @@ if (message.content.startsWith(adminprefix + 'ls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
 } else     
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : Done :>`)
-return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
+ 
 if (message.content.startsWith(adminprefix + 'st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`) //حقوق GMZN Host
 }
-
 });
-
 
 client.login(process.env.BOT_TOKEN);
